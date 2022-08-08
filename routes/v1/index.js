fc47d980f9362613d18f24095eada7331fdcc804
Tayyab <heyitstay011@gmail.com>
2022-08-08 22:31:39 +0530

    evnts

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,6 +3,7 @@ import authRoutes from './auth.js';
 import userRoutes from './user.js';
 import playlistRoutes from './playlist.js';
 import videoRoutes from './video.js';
+import eventRoutes from './event.js';
 
 const router = Router();
 
@@ -14,6 +15,7 @@ router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/playlist', playlistRoutes);
 router.use('/video', videoRoutes);
+router.use('/event', eventRoutes);
 
 
 export default router;
